d709f91e7ddb2c5fda26c916e476fae7bc2efddb
Aritajames <arita.jemo20@gmail.com>
2019-07-17 11:47:21 +0300

    initial commit

--- login_handler.php
+++ login_handler.php
@@ -1,13 +1,36 @@
-<?php include 'config.php';
-
+<?php
 session_start();
-
-$sql = "SELECT  `email`, `password` FROM `project`";
-
-if(mysqli_query($conn,$sql)){
-    header('location:index.php');
-    $_SESSION['email']=$email;
-}else{
-    header('location:signup.php');
+#Declare/initialize empty variables to store the recieved data
+$email=$password='';
+include "config.php";
+if(isset($_POST['signin'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    if(empty($email)){
+        header('location:login.php?e_error');
+        exit();
+    }else{
+        $email = $email;
+    }
+    if(empty($password)){
+        header('location:login.php?p_error');
+        exit();
+    }else{
+        $password = $password;
+    }
+    $sql = "SELECT * FROM `project` WHERE email='$email' LIMIT 1";
+    $results = mysqli_query($conn,$sql);
+    $user = mysqli_fetch_assoc($results);
+    if($user){
+        if($email == $email){
+            $_SESSION['loggedin'] = true;
+            header('location:index.php?success_msg');
+            $_SESSION['email'] = $email;
+            header('location:index.php');
+        }
+    }else{
+        header('location:signup.php?user_msg');
+        exit();
+    }
 }
 ?>
